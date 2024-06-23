7979f01b75521d0df68bc92599fd0195ae5292a0
Naina <nainachhapola2706@gmail.com>
2024-06-23 11:41:00 +0530

    edit contact file

--- frontend/src/Home/contact.jsx
+++ frontend/src/Home/contact.jsx
@@ -55,21 +55,22 @@ export default function Contact() {
 
               <div className="social-media">
                 <h3>Follow Us :</h3>
-                <button>
-                  <Link to="https://www.instagram.com/vishaljoshi.vjsir?igsh=MXdpaDh0anYzZWphOA==">
-                    <i class="fa-brands fa-2x fa-instagram"></i>
-                  </Link>
-                </button>
-                <button class="fa-brands fa-2x fa-youtube"></button>
-                <button>
-                  <Link to="https://www.linkedin.com/in/vishaljoshivjsir?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BsU%2Fw8UrWT0GlOYG2sv9vuw%3D%3D">
-                    <i class="fa-brands fa-2x fa-linkedin"></i>
-                  </Link>
-                </button>
-                <button class="fa-brands fa-2x fa-facebook"></button>
+                <div className="social-media-btn">
+                  <button>
+                    <Link to="https://www.instagram.com/vishaljoshi.vjsir?igsh=MXdpaDh0anYzZWphOA==">
+                      <i class="fa-brands fa-2x fa-instagram"></i>
+                    </Link>
+                  </button>
+                  <button class="fa-brands fa-2x fa-youtube"></button>
+                  <button>
+                    <Link to="https://www.linkedin.com/in/vishaljoshivjsir?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BsU%2Fw8UrWT0GlOYG2sv9vuw%3D%3D">
+                      <i class="fa-brands fa-2x fa-linkedin"></i>
+                    </Link>
+                  </button>
+                  <button class="fa-brands fa-2x fa-facebook"></button>
+                </div>
               </div>
             </div>
-
             <div className="contact-right">
               <h1>Send a Message</h1>
 
