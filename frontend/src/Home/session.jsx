c27cee173dd5a8aeb2bec2f03e8728ed5586bbb1
Naina <nainachhapola2706@gmail.com>
2024-06-23 05:56:53 +0530

    delete unused images

--- frontend/src/Home/session.jsx
+++ frontend/src/Home/session.jsx
@@ -1,32 +1,31 @@
-import "./session.css"
-import Sessionimg from "../assets/images/session book.png";
-export default function Session(){
-return (
-  <>
-    <section id="Session">
-      <div className="session">
-        
-        <img src={Sessionimg} alt="" />
-
-        <div className="session-right">
-          <div className="session-text">
-            <h2>Book 1:1 Session</h2>
-            <h3>Creative & Professional Digital Mentors!</h3>
-            <p>
-              Students feel at ease during personalized 1-1 mentor sessions for
-              a demo. They value the tailored guidance that meets their unique
-              needs and questions. This individualized attention boosts their
-              confidence and understanding. It creates a safe space for open
-              communication and feedback. Overall, these sessions are more
-              engaging and effective for achieving their learning goals.
-            </p>
+import "./session.css";
+import Sessionimg from "../assets/images/sneha.png";
+export default function Session() {
+  return (
+    <>
+      <section id="Session">
+        <div className="session">
+          <div className="session-left">
+            <img src={Sessionimg} alt="" />
           </div>
+          <div className="session-right">
+            <div className="session-text">
+              <h2>Book 1:1 Session</h2>
+              <h3>Creative & Professional Digital Mentors!</h3>
+              <p>
+                Students feel at ease during personalized 1-1 mentor sessions
+                for a demo. They value the tailored guidance that meets their
+                unique needs and questions. This individualized attention boosts
+                their confidence and understanding. It creates a safe space for
+                open communication and feedback. Overall, these sessions are
+                more engaging and effective for achieving their learning goals.
+              </p>
+            </div>
 
-          <button className="session-btn">BUY NOW</button>
+            <button className="session-btn">BUY NOW</button>
+          </div>
         </div>
-
-      </div>
-    </section>
-  </>
-);
+      </section>
+    </>
+  );
 }
