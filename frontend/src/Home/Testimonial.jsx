14594d72f64312c12bda49024b1ffa08b78a4bc4
Naina <nainachhapola2706@gmail.com>
2024-06-23 08:59:38 +0530

    update testimonial

--- frontend/src/Home/Testimonial.jsx
+++ frontend/src/Home/Testimonial.jsx
@@ -1,56 +1,113 @@
+import React from "react";
 import "./Testimonial.css";
-import "./TestimonialCard.jsx"
 import TestimonialCard from "./TestimonialCard.jsx";
+import Testimonialcard from "../assets/images/testimonial.png";
+
+
 export default function Testimonial() {
+  const testimonials = [
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "...Moreover getting connected felt so easy and I was able to discuss even the minutest problems and find the right approach to resolve them. But the best part about this is I have been able to discuss and get the most genuine suggestions for any other problems besides academics, which not only helps me stay motivated but has also helped me in getting more consistent and disciplined with my routine.",
+
+      name: "Shriya",
+      designation: "AIR 1, JEE ADVANCED 2014",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "..kya main apne dream ko pahunch paunga yeh sawal hamesha raheta tha aur jab maine Sneha didi ki jee story yt se dekhi toh I feel ki yaar meri problems toh inki problems ke saamne kuch hai hi nahi aur Sneha didi se zoom meetings and google meet pe baat karke ek fire chali ki nahi ab toh mai puri mhenat karunga, and from that day and with Sneha didi's mentorship talks I  started to study consistently. In Brief, She is an amazing mentor you can get from this mentorship batch.",
+
+      name: "Sumeet Kumar",
+      designation: "Topper, NEET 2020",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "..She makes my study planner and give me the solution of my distractions which is very helpful for me....She is very kind and helpful whenever I am in problem she gives me the best solution...My self study efficiency is increasing day by day and the number mistakes that I made in last days become less day by day..",
+
+      name: "Kartavya Gupta",
+      designation: "Topper, NEET 2020",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "..When I didn't take the mentorship than I  am not able to study properly at the start I was so distracted but when I take the mentorship then I attend the meeting with sneha didi and she motivates me and develops a confidence in me.THANK YOU DIDI",
+
+      name: " Adarsh",
+      designation: "Topper, NEET 2020",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "...Moreover getting connected felt so easy and I was able to discuss even the minutest problems and find the right approach to resolve them. But the best part about this is I have been able to discuss and get the most genuine suggestions for any other problems besides academics, which not only helps me stay motivated but has also helped me in getting more consistent and disciplined with my routine.",
+
+      name: "Shriya",
+      designation: "AIR 1, JEE ADVANCED 2014",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "..kya main apne dream ko pahunch paunga yeh sawal hamesha raheta tha aur jab maine Sneha didi ki jee story yt se dekhi toh I feel ki yaar meri problems toh inki problems ke saamne kuch hai hi nahi aur Sneha didi se zoom meetings and google meet pe baat karke ek fire chali ki nahi ab toh mai puri mhenat karunga, and from that day and with Sneha didi's mentorship talks I  started to study consistently. In Brief, She is an amazing mentor you can get from this mentorship batch.",
+
+      name: "Sumeet Kumar",
+      designation: "Topper, NEET 2020",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "..She makes my study planner and give me the solution of my distractions which is very helpful for me....She is very kind and helpful whenever I am in problem she gives me the best solution...My self study efficiency is increasing day by day and the number mistakes that I made in last days become less day by day..",
+
+      name: "Kartavya Gupta",
+      designation: "Topper, NEET 2020",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "..When I didn't take the mentorship than I  am not able to study properly at the start I was so distracted but when I take the mentorship then I attend the meeting with sneha didi and she motivates me and develops a confidence in me.THANK YOU DIDI",
+
+      name: " Adarsh",
+      designation: "Topper, NEET 2020",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "...Moreover getting connected felt so easy and I was able to discuss even the minutest problems and find the right approach to resolve them. But the best part about this is I have been able to discuss and get the most genuine suggestions for any other problems besides academics, which not only helps me stay motivated but has also helped me in getting more consistent and disciplined with my routine.",
+
+      name: "Shriya",
+      designation: "AIR 1, JEE ADVANCED 2014",
+    },
+    {
+      cardImage: Testimonialcard,
+      testimonialText:
+        "..kya main apne dream ko pahunch paunga yeh sawal hamesha raheta tha aur jab maine Sneha didi ki jee story yt se dekhi toh I feel ki yaar meri problems toh inki problems ke saamne kuch hai hi nahi aur Sneha didi se zoom meetings and google meet pe baat karke ek fire chali ki nahi ab toh mai puri mhenat karunga, and from that day and with Sneha didi's mentorship talks I  started to study consistently. In Brief, She is an amazing mentor you can get from this mentorship batch.",
+
+      name: "Sumeet Kumar",
+      designation: "Topper, NEET 2020",
+    },
+    // Add more testimonials as needed
+  ];
+
   return (
-    <>
-      <section id="Testimonial">
-        <h1>What Our Students Say?</h1>
-
-        <div className="Testimonial-slider">
-          <div className="Testimonial-slide-track">
-            <div className="Testimonial-slide">
-              <TestimonialCard />
+    <section id="Testimonial">
+      <h1>What Our Students Say?</h1>
+      <div className="Testimonial-slider">
+        <div className="Testimonial-slide-track">
+          {testimonials.map((testimonial, index) => (
+            <div className="Testimonial-slide" key={index}>
+              <TestimonialCard
+                cardImage={testimonial.cardImage}
+                testimonialText={testimonial.testimonialText}
+                // personImage={testimonial.personImage}
+                name={testimonial.name}
+                designation={testimonial.designation}
+              />
             </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-
-            <div className="Testimonial-slide">
-              <TestimonialCard />
-            </div>
-          </div>
+          ))}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
+
--- frontend/src/Home/TestimonialCard.jsx
+++ frontend/src/Home/TestimonialCard.jsx
@@ -1,30 +1,20 @@
+import React from "react";
 import "./TestimonialCard.css";
 import Testimonialcard from "../assets/images/testimonial.png";
-import Testimonialimg from "../assets/images/testimonialimg.png";
-export default function TestimonialCard() {
-  return (
-    <>
-      <div className="Testimonial-cards">
-        <img src={Testimonialcard} alt="" />
-        <div className="testimonial-content">
-          <p>
-            Achieving AIR Rank 1 in JEE was a dream come true, made possible by
-            VJ sir's unparalleled guidance and dedication. His deep
-            understanding and innovative teaching methods inspired and equipped
-            me to excel beyond my expectations.
-          </p>
 
-          <img
-            className=" testimonial-img"
-            src={Testimonialimg}
-            alt=""
-          />
-          <div className="testimonial-text">
-            <h2>Chitraang Murdia</h2>
-            <h3>-AIR 1, JEE ADVANCED 2014</h3>
-          </div>
+export default function TestimonialCard({ cardImage, testimonialText, personImage, name, designation }) {
+  return (
+    <div className="Testimonial-cards">
+      <img src={Testimonialcard} alt="" />
+      <div className="testimonial-content">
+        <p>{testimonialText}</p>
+        {/* <img className="testimonial-img" src={personImage} alt="" /> */}
+        <div className="testimonial-text">
+          <h2>{name}</h2>
+          {/* <h3>{designation}</h3> */}
         </div>
       </div>
-    </>
+    </div>
   );
 }
+
