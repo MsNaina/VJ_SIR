295a919c2f7634338726fd0216522d36dc3c8152
Naina <nainachhapola2706@gmail.com>
2024-06-24 06:07:50 +0530

    responsive profile page

--- frontend/src/Mentorship/Navbar.jsx
+++ frontend/src/Mentorship/Navbar.jsx
@@ -30,6 +30,7 @@ export default function Navbar() {
   return (
     <>
       <section id="Navbar">
+        
         <div className="logo">
           <img src={Logo} alt="Logo" />
         </div>
--- frontend/src/profilepage/profile.jsx
+++ frontend/src/profilepage/profile.jsx
@@ -1,72 +1,113 @@
 import "./profile.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate} from "react-router-dom";
 import Logo from "../assets/images/logo.png";
-import Profileimg from "../assets/images/profile1.png";
 import info from "../assets/images/profile-info.png";
+import React, { useState, useEffect } from "react";
 export default function Profile() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const auth = localStorage.getItem("token");
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState("");
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (user) {
+      setUserName(user.name);
+    }
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    navigate("/Signup");
+  };
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
-      <section id="Profile">
-        <div className="profile-top">
-          <div className="profile-logo">
-            <img src={Logo} alt="" />
+      <section id="user-Profile">
+        <div className="user-profile">
+          <div className="profile-top">
+            <div className="profile-logo">
+              <img src={Logo} alt="" />
+            </div>
+
+            <div className={`menu ${menuOpen ? "open" : ""}`}>
+              <div className="profile-Nav-item">
+                <ul>
+                  <li>
+                    <NavLink className="links" to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink to="/Resource">Resource</NavLink>
+                  </li>
+                  <li>
+                    <NavLink className=" links" to="/Mentorship">
+                      Mentorship
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink className="links" to="/AboutUs">
+                      About Us
+                    </NavLink>
+                  </li>
+                  <li>
+                    {auth ? (
+                      <NavLink onClick={logout} to="/Signup">
+                        Logout
+                      </NavLink>
+                    ) : (
+                      <NavLink to="/login" onClick={toggleMenu}>
+                        Login
+                      </NavLink>
+                    )}
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div
+              className={`hamburger ${menuOpen ? "open" : ""}`}
+              onClick={toggleMenu}
+            >
+              <div className="line"></div>
+              <div className="line"></div>
+              <div className="line"></div>
+            </div>
           </div>
 
-          <div className="profile-Nav-item">
-            <ul>
-              <li>
-                <NavLink className="links" to="/">
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink to="/Resource">Resource</NavLink>
-              </li>
-              <li>
-                <NavLink className=" links" to="/Mentorship">
-                  Mentorship
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="links" to="/AboutUs">
-                  About Us
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="links" to="/Login">
-                  Login
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="profile-bottom">
-          <div className="profile-left">
-            <img className="profile-img" src={Profileimg} alt="" />
-          </div>
-
-          <div className="profile-right">
+          <div className="profile-bottom">
             <h2>Profile</h2>
 
             <div className="profile-info">
               <img src={info} alt="" />
-              <h3>Name</h3>
+              <h3>
+                {userName ? (
+                  <NavLink to="/profile">{userName}</NavLink>
+                ) : (
+                  <NavLink to="/profile">Profile</NavLink>
+                )}
+              </h3>
             </div>
 
             <div>
               <h2>Personal Information</h2>
               <div className="personal-info">
-                <div>
+                <div className="profile-label">
                   <label htmlFor="">Mobile No.</label>
-                  <input id="profile-input" type="tel"/>
+                  <input id="profile-input" type="tel" />
                 </div>
-                <div>
+                <div className="profile-label">
                   <label htmlFor="">Email ID</label>
                   <input id="profile-input" type="email" />
                 </div>
-               
-                <div>
+
+                <div className="profile-label">
                   <label htmlFor="">City</label>
                   <input id="profile-input" type="text" />
                 </div>
